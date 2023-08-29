794ccf78561353e3d1d706e5e222f75e51a301bc
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-29 11:57:56 +0500

    callaback query

--- inlinebot.py
+++ inlinebot.py
@@ -7,14 +7,22 @@ def start(update: Update, context: CallbackContext):
     bot = context.bot
     chat_id = update.message.chat.id
 
-    button1 = InlineKeyboardButton(text="button1", callback_data='1')
+    button1 = InlineKeyboardButton(text="button1", callback_data='button__1')
     button2 = InlineKeyboardButton(text="button2", callback_data="2")
     keyboard =InlineKeyboardMarkup([[button1, button2]])
 
     bot.sendMessage(chat_id,text = "Inline Keyboard", reply_markup=keyboard)
 
 def main(update, context):
-    print(1)
+
+    query = update.callback_query
+    data = query.data
+        
+    button1 = InlineKeyboardButton(text="button1", callback_data='button__1')
+    button2 = InlineKeyboardButton(text="button2", callback_data="2")
+    keyboard =InlineKeyboardMarkup([[button1, button2]])
+
+    query.edit_message_text(text='main', reply_markup = keyboard)
 
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler(CommandHandler('start', start))
