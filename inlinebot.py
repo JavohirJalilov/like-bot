8b4a87c8ec0ce414226543b90ed7ce726f3f5107
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-29 11:48:07 +0500

    create inline button

---
+++ inlinebot.py
@@ -0,0 +1,23 @@
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler, CallbackQueryHandler
+TOKEN ='5892121487:AAFJ8hXhSsCFBNMp-hHqFtAfwhO8RtCxdrM'
+
+
+def start(update: Update, context: CallbackContext):
+    bot = context.bot
+    chat_id = update.message.chat.id
+
+    button1 = InlineKeyboardButton(text="button1", callback_data='1')
+    button2 = InlineKeyboardButton(text="button2", callback_data="2")
+    keyboard =InlineKeyboardMarkup([[button1, button2]])
+
+    bot.sendMessage(chat_id,text = "Inline Keyboard", reply_markup=keyboard)
+
+def main(update, context):
+    print(1)
+
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler((CallbackQueryHandler(main)))
+updater.start_polling()
+updater.idle()
